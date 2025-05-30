2ef1bee50e2546f1707e446574cd94b5b1f92290
txmlrd <gungadhisanjaya@gmail.com>
2025-05-30 16:09:14 +0700

    feat: update api-gateway on teacher class detail

--- app/routes/syukra/teacher/teacher.py
+++ app/routes/syukra/teacher/teacher.py
@@ -248,82 +248,161 @@ def get_class_detail_all_week():
         return jsonify(response.json()), response.status_code
     except requests.exceptions.RequestException as e:
         return jsonify({"error": "Class Control Service unavailable", "details": str(e)}), 503
-
-@syukra_teacher_bp.route('/item-pembelajaran/<uuid>', methods=['DELETE'])
+    
+@syukra_teacher_bp.route('/kelas', methods=['GET'])
 @jwt_required()
 @check_device_token
 @check_permission('class_detail')
-def delete_item_pembelajaran_by_uuid(uuid):
+def get_class_detail_by_id():
     try:
-        response = requests.delete(f"{Config.URL_CONTENT}/item-pembelajaran/{uuid}")
+        response = requests.get(f"{Config.URL_CLASS_CONTROL}/kelas", params=request.args)
+        return jsonify(response.json()), response.status_code
+    except requests.exceptions.RequestException as e:
+        return jsonify({"error": "Class Control Service unavailable", "details": str(e)}), 503
+
+@syukra_teacher_bp.route('/item-pembelajaran/', methods=['DELETE'])
+@jwt_required()
+@check_device_token
+@check_permission('class_detail')
+def delete_item_pembelajaran_by_uuid():
+    try:
+        response = requests.delete(f"{Config.URL}/item-pembelajaran", params=request.args)
         return jsonify(response.json()), response.status_code
     except requests.exceptions.RequestException as e:
         return jsonify({"error": "Class Service unavailable", "details": str(e)}), 503
 
-@syukra_teacher_bp.route('/item-pembelajaran/<uuid>', methods=['GET'])
+@syukra_teacher_bp.route('/item-pembelajaran/', methods=['GET'])
 @jwt_required()
 @check_device_token
 @check_permission('class_detail')
-def get_item_pembelajaran_by_uuid(uuid):
+def get_item_pembelajaran_by_uuid():
     try:
-        response = requests.get(f"{Config.URL_CONTENT}/item-pembelajaran/{uuid}")
+        response = requests.get(f"{Config.URL}/item-pembelajaran", params=request.args)
         return jsonify(response.json()), response.status_code
     except requests.exceptions.RequestException as e:
         return jsonify({"error": "Class Service unavailable", "details": str(e)}), 503
-
-@syukra_teacher_bp.route('/item-pembelajaran/', methods=['POST'])
-@jwt_required()
-@check_device_token
-@check_permission('class_detail')
-def upload_file_pembelajaran():
-    file = request.files.get('file')
-    try:
-        response = requests.post(f"{Config.URL_CONTENT}/item-pembelajaran", files=file)
-        return jsonify(response.json()), response.status_code
-    except requests.exceptions.RequestException as e:
-        return jsonify({"error": "Class Content Service unavailable", "details": str(e)}), 503
-
+    
 @syukra_teacher_bp.route('/teacher/kelas/weekly-section', methods=['POST'])
 @jwt_required()
 @check_device_token
 @check_permission('class_detail')
 def create_weekly_section():
-    data = request.get_json()
+    data = request.form.to_dict()
+    file = request.files.get('file')
+
     if not data:
         return jsonify({"error": "Invalid input"}), 400
-    
+
+    # Konversi week_number ke integer jika ada
+    if 'week_number' in data:
+        try:
+            data['week_number'] = int(data['week_number'])
+        except ValueError:
+            return jsonify({"error": "week_number must be an integer"}), 400
     try:
-        response = requests.post(f"{Config.URL_CLASS_CONTROL}/teacher/kelas/weekly-section", json=data)
+        files = {'file': (file.filename, file.stream, file.mimetype)} if file else None
+        response = requests.post(
+            f"{Config.URL_CLASS_CONTROL}/teacher/kelas/weekly-section",
+            data=data,
+            files=files
+        )
         return jsonify(response.json()), response.status_code
     except requests.exceptions.RequestException as e:
-        return jsonify({"error": "Class Control Service unavailable", "details": str(e)}), 503
-      
-@syukra_teacher_bp.route('/item-pembelajaran/assignment', methods=['POST'])
+        return jsonify({
+            "error": "Class Control Service unavailable",
+            "details": str(e)
+        }), 503
+        
+@syukra_teacher_bp.route('/teacher/kelas/weekly-section', methods=['PUT'])
 @jwt_required()
 @check_device_token
 @check_permission('class_detail')
-def upload_file_assignment():
+def update_weekly_section_teacher():
+    data = request.form.to_dict()
     file = request.files.get('file')
+
+    if not data:
+        return jsonify({"error": "Invalid input"}), 400
+
+    files = {'file': (file.filename, file.stream, file.mimetype)} if file else None
+
     try:
-        response = requests.post(f"{Config.URL_CONTENT}/item-pembelajaran/assignment", files=file)
+        response = requests.put(
+            f"{Config.URL_CLASS_CONTROL}/teacher/kelas/weekly-section",
+            data=data,
+            files=files,
+        )
         return jsonify(response.json()), response.status_code
     except requests.exceptions.RequestException as e:
-        return jsonify({"error": "Class Content Service unavailable", "details": str(e)}), 503
+        return jsonify({
+            "error": "Class Control Service unavailable",
+            "details": str(e)
+        }), 503
+
+@syukra_teacher_bp.route('/teacher/kelas/assignment', methods=['PUT'])
+@jwt_required()
+@check_device_token
+@check_permission('class_detail')
+def update_assignment_teacher():
+    data = request.form.to_dict()
+    file = request.files.get('file')
+
+    if not data:
+        return jsonify({"error": "Invalid input"}), 400
+    
+    files = {'file': (file.filename, file.stream, file.mimetype)} if file else None
+
+    try:
+        response = requests.put(
+            f"{Config.URL_CLASS_CONTROL}/teacher/kelas/assignment",
+            data=data,
+            files=files,
+        )
+        return jsonify(response.json()), response.status_code
+    except requests.exceptions.RequestException as e:
+        return jsonify({
+            "error": "Class Control Service unavailable",
+            "details": str(e)
+        }), 503
+
+from werkzeug.utils import secure_filename
 
 @syukra_teacher_bp.route('/teacher/kelas/assignment', methods=['POST'])
 @jwt_required()
 @check_device_token
 @check_permission('class_detail')
 def create_assignment():
-    data = request.get_json()
+    data = request.form.to_dict()
+    file = request.files.get('file')
+
     if not data:
         return jsonify({"error": "Invalid input"}), 400
-    
+
+    # Siapkan file untuk dikirim via requests
+    files = None
+    if file:
+        files = {
+            'file': (secure_filename(file.filename), file.stream, file.mimetype)
+        }
+
+    # Ambil Authorization token dari header
+    auth_header = request.headers.get("Authorization")
+    if not auth_header:
+        return jsonify({"error": "Authorization header missing"}), 401
+
     try:
-        response = requests.post(f"{Config.URL_CLASS_CONTROL}/teacher/kelas/assignment", json=data)
+        response = requests.post(
+            f"{Config.URL_CLASS_CONTROL}/teacher/kelas/assignment",
+            data=data,
+            files=files,
+            headers={"Authorization": auth_header}
+        )
         return jsonify(response.json()), response.status_code
     except requests.exceptions.RequestException as e:
-        return jsonify({"error": "Class Control Service unavailable", "details": str(e)}), 503
+        return jsonify({
+            "error": "Class Control Service unavailable",
+            "details": str(e)
+        }), 503
 
 @syukra_teacher_bp.route('/teacher/kelas/weekly-section', methods=['DELETE'])
 @jwt_required()
@@ -335,3 +414,4 @@ def delete_weekly_section():
         return jsonify(response.json()), response.status_code
     except requests.exceptions.RequestException as e:
         return jsonify({"error": "Class Control Service unavailable", "details": str(e)}), 503
+    
