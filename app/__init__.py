ed54ddd4e5c5902a0c4653f460aa4b2cd5ad7e7d
txmlrd <gungadhisanjaya@gmail.com>
2025-06-08 22:16:25 +0700

    fix: add cors origin domain spesific

--- app/__init__.py
+++ app/__init__.py
@@ -13,7 +13,10 @@ from flask_cors import CORS
 
 def create_app():
     app = Flask(__name__)
-    CORS(app)
+    CORS(app, resources={r"/*": {"origins": [
+    "https://team-edu.vercel.app"
+    ]}}, supports_credentials=True)
+
     app.config.from_object('config.Config')
     
     jwt.init_app(app)
